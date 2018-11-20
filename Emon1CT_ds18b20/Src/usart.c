918832c02319599f5a8789cb078a23ff03714abc
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-11-20 16:47:55 +0000

    attempt at ds18b20 support (not working)

---
+++ Emon1CT_ds18b20/Src/usart.c
@@ -0,0 +1,393 @@
+/**
+  ******************************************************************************
+  * File Name          : USART.c
+  * Description        : This file provides code for the configuration
+  *                      of the USART instances.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "usart.h"
+
+#include "gpio.h"
+#include "dma.h"
+
+/* USER CODE BEGIN 0 */
+
+#include <string.h>
+
+#define ONE_WIRE_RESET_PULSE 0xF0
+#define ONE_WIRE_ONE_VALUE  0xFF
+#define ONE_WIRE_ZERO_VALUE 0x00
+
+#define MAX_1WIRE_DMA  12*8           // 1 DMA byte holds one OneWire bit, so 12 bytes here
+static volatile uint8_t rx_buff[MAX_1WIRE_BUS][MAX_1WIRE_DMA];
+static uint8_t tx_buff[MAX_1WIRE_BUS][MAX_1WIRE_DMA];
+volatile uint8_t tx_busy[MAX_1WIRE_BUS];
+
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart1_rx;
+DMA_HandleTypeDef hdma_usart1_tx;
+
+/* USART1 init function */
+
+void MX_USART1_UART_Init(void)
+{
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 38400;
+  huart1.Init.WordLength = UART_WORDLENGTH_7B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_HalfDuplex_Init(&huart1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+/* USART2 init function */
+
+void MX_USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspInit 0 */
+
+  /* USER CODE END USART1_MspInit 0 */
+    /* USART1 clock enable */
+    __HAL_RCC_USART1_CLK_ENABLE();
+  
+    /**USART1 GPIO Configuration    
+    PA9     ------> USART1_TX 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /* USART1 DMA Init */
+    /* USART1_RX Init */
+    hdma_usart1_rx.Instance = DMA1_Channel5;
+    hdma_usart1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_usart1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart1_rx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart1_rx.Init.Mode = DMA_NORMAL;
+    hdma_usart1_rx.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_usart1_rx) != HAL_OK)
+    {
+      _Error_Handler(__FILE__, __LINE__);
+    }
+
+    __HAL_LINKDMA(uartHandle,hdmarx,hdma_usart1_rx);
+
+    /* USART1_TX Init */
+    hdma_usart1_tx.Instance = DMA1_Channel4;
+    hdma_usart1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+    hdma_usart1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_usart1_tx.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_usart1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+    hdma_usart1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+    hdma_usart1_tx.Init.Mode = DMA_NORMAL;
+    hdma_usart1_tx.Init.Priority = DMA_PRIORITY_LOW;
+    if (HAL_DMA_Init(&hdma_usart1_tx) != HAL_OK)
+    {
+      _Error_Handler(__FILE__, __LINE__);
+    }
+
+    __HAL_LINKDMA(uartHandle,hdmatx,hdma_usart1_tx);
+
+  /* USER CODE BEGIN USART1_MspInit 1 */
+
+  /* USER CODE END USART1_MspInit 1 */
+  }
+  else if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspInit 0 */
+
+  /* USER CODE END USART2_MspInit 0 */
+    /* USART2 clock enable */
+    __HAL_RCC_USART2_CLK_ENABLE();
+  
+    /**USART2 GPIO Configuration    
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART2_MspInit 1 */
+
+  /* USER CODE END USART2_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
+
+  if(uartHandle->Instance==USART1)
+  {
+  /* USER CODE BEGIN USART1_MspDeInit 0 */
+
+  /* USER CODE END USART1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART1_CLK_DISABLE();
+  
+    /**USART1 GPIO Configuration    
+    PA9     ------> USART1_TX 
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9);
+
+    /* USART1 DMA DeInit */
+    HAL_DMA_DeInit(uartHandle->hdmarx);
+    HAL_DMA_DeInit(uartHandle->hdmatx);
+  /* USER CODE BEGIN USART1_MspDeInit 1 */
+
+  /* USER CODE END USART1_MspDeInit 1 */
+  }
+  else if(uartHandle->Instance==USART2)
+  {
+  /* USER CODE BEGIN USART2_MspDeInit 0 */
+
+  /* USER CODE END USART2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_USART2_CLK_DISABLE();
+  
+    /**USART2 GPIO Configuration    
+    PA2     ------> USART2_TX
+    PA3     ------> USART2_RX 
+    */
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_2|GPIO_PIN_3);
+
+  /* USER CODE BEGIN USART2_MspDeInit 1 */
+
+  /* USER CODE END USART2_MspDeInit 1 */
+  }
+} 
+
+/* USER CODE BEGIN 1 */
+
+//
+// OneWire bus 0 runs on uart1 and bus 1 runs on uart3.  These two
+// functions map between the two.  If there are ever more than
+// two OneWire buses they'll need a mention in here.
+//
+static inline UART_HandleTypeDef* bus_to_huart (int bus) {
+
+  return &huart1;
+}
+static inline int huart_to_bus (UART_HandleTypeDef *huart) {
+
+  if (huart == &huart1)
+    return 0;
+  else
+    return 1;
+}
+
+void HAL_UART_TxCpltCallback (UART_HandleTypeDef *huart) {
+
+  HAL_UART_DMAStop(huart);
+  tx_busy[huart_to_bus(huart)] = 0;          // Flag that the tx path is now idle
+}
+
+void onewire_tx (int bus, uint8_t* p, int len) {
+  int tx_i = 0;
+  
+  if (len*8 > sizeof(tx_buff)) return;
+  while (tx_busy[bus]);                         // Wait for previous transaction to complete
+
+  tx_busy[bus] = 1;                          // Flag it as busy again for this transaction
+  for (int i=0; i<len; i++) {
+    uint8_t outb = *p++;
+    for (int b=0; b<8; b++) {
+      tx_buff[bus][tx_i++] = (outb & 0x01) ? ONE_WIRE_ONE_VALUE : ONE_WIRE_ZERO_VALUE;
+      outb >>= 1;
+    }
+  }
+  HAL_UART_Transmit_DMA(bus_to_huart(bus), tx_buff[bus], tx_i);
+}
+
+//
+// Receiving a large number of bytes can take a while.  For example, reading the 9 byte
+// scratchpad in the DS18B20 takes ~6 msecs.  Rather than have the caller block for the duration
+// we split this fuction into two parts (rx1 and rx2).  rx1 starts the burst read running
+// and returns (subject to first waiting for any prior transactions to complete).  rx2 waits
+// for it to complete and returns the result.  Callers who have nothing better to do and just
+// want to wait can simply call rx1 and rx2 back-to-back.
+//
+void onewire_rx1 (int bus, uint8_t* p, int len) {
+  int bit_count = len*8;
+  UART_HandleTypeDef *huart = bus_to_huart(bus);
+  
+  if (bit_count > MAX_1WIRE_DMA) return;      // Will it fit?
+  while (tx_busy[bus]);                       // Wait for previous transactions to complete
+
+  //
+  // We need to output a 0xff for each OneWIre bit we want to receive
+  //
+  tx_busy[bus] = 1;
+  memset(tx_buff[bus], 0xff, bit_count);                             // tx 0xff is a read slot
+  HAL_UART_Receive(huart, (uint8_t*)rx_buff[bus], MAX_1WIRE_DMA, 0); // Flush all outstanding rx bytes
+  HAL_UART_Receive_DMA(huart, (uint8_t*)rx_buff[bus], MAX_1WIRE_DMA);// Prepare for incoming
+  HAL_UART_Transmit_DMA(huart, tx_buff[bus], bit_count);             // Start sending out the read slots
+
+  //
+  // We've kicked it all off, so part1 returns now so the caller can get on with
+  // other stuff while it's arriving.  If they've got nothing else to do, they can
+  // immediately call part2 which will block until it's all arrived.
+  //
+  return;
+}
+
+void onewire_rx2 (int bus, uint8_t* p, int len) {
+  int bit_count = len*8;
+  int rx_i;
+  
+  if (bit_count > MAX_1WIRE_DMA) return;
+  while (tx_busy[bus]);                                           // Wait for part1 to complete
+
+  //
+  // By now all the requested read slots have been tx'd and the results should be back in rx_buff.
+  // We need to turn the "bits" in rx_buff back into bytes in the caller supplied buffer.
+  // The data comes in LSB first.
+  //
+  rx_i = 0;
+
+  //
+  // For each byte the caller has requested
+  //
+  for (int byte_i=0; byte_i<len; byte_i++){
+    *p = 0;
+
+    //
+    // For each bit in this caller byte
+    //
+    for (int bit_i=0; bit_i<8; bit_i++) {
+      *p >>= 1;                                   // Make room for new bit (LSB first)
+      if (rx_buff[bus][rx_i++] == ONE_WIRE_ONE_VALUE)
+	*p |= 0x80;
+    }
+    p++;                                          // Move on to the next caller byte
+  }
+}
+
+//
+// See Maxim's Tutorial 214 for details of how all this UART timing stuff works.
+// The bus reset happens at 9600 baud and everything else happens at 115200 baud.
+// One byte through the UART represents one bit on the OneWire.
+//
+uint8_t onewire_reset (int bus) {
+  UART_HandleTypeDef *huart = bus_to_huart(bus);
+
+  while (tx_busy[bus]);                          // Wait for previous transactions to complete
+  huart->Init.BaudRate = 9600;
+  HAL_HalfDuplex_Init(huart);
+  HAL_UART_Receive(huart, (uint8_t*)rx_buff[bus], MAX_1WIRE_DMA, 0);  // Flush all outstanding rx bytes
+
+  tx_buff[bus][0] = ONE_WIRE_RESET_PULSE;
+  tx_busy[bus] = 1;
+
+  //
+  // Because of the uart reset just above (to change the baud rate), the tx path doesn't
+  // become ready for 1 byte time (~1 msec) so the read back needs to wait sufficiently
+  // long for the transmit to happen.
+  //
+  HAL_UART_Transmit_DMA(huart, tx_buff[bus], 1);
+  HAL_UART_Receive(huart, (uint8_t*)rx_buff[bus], 1, 5);    // 1 byte, 5 msecs timeout
+
+  huart->Init.BaudRate = 115200;
+  HAL_HalfDuplex_Init(huart);
+
+  return rx_buff[bus][0];
+}
+
+//void debug_printf (char* p) {
+  //HAL_UART_Transmit(&huart2, (uint8_t*)p, strlen(p), 1000);
+//}
+
+void init_uarts (void) {
+
+}
+
+void debug_printf (char* p) {
+  HAL_UART_Transmit(&huart2, (uint8_t*)p, strlen(p), 1000);
+}
+/* USER CODE END 1 */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
